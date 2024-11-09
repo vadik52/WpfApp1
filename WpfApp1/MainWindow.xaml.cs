39b37da932214e4e65519ab8896ae8dadf10be04
admin 10 <chillboot3d@gmail.com>
2024-11-09 15:02:22 +1000

    add menu ²ьте файлы проекта.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string imgsourcel1 = @"C:\kakaha113.png";
+        private string imgsourcel2 = @"C:\rikardo.gif";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,6 +33,22 @@ namespace WpfApp1
             win2 window = new win2();
             window.Show();
             this.Close();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("приложение закрылось");
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            DisplayImage.Source = new BitmapImage(new Uri(imgsourcel1));
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            DisplayImage.Source = new BitmapImage(new Uri(imgsourcel2));
         }
     }
 }
